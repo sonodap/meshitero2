cc348f909f5d3471d53fcaf3abbe5aad326ee3d0
sonodap <t106r61@gmail.com>
2023-05-05 06:49:18 +0000

    投稿フォームの作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,7 +1,26 @@
 class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_prame)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_image_path
+  end
+
   def index
   end
 
   def show
+  end
+
+
+  private
+
+  def post_image_params
+    parame.require(:post_image).permit(:shop.name, :image, :caption)
   end
 end
