0294ec8ea3c15b78c145b843836d820770d7a5f5
sonodap <t106r61@gmail.com>
2023-05-08 07:24:57 +0000

    投稿機能の修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,10 +5,10 @@ class PostImagesController < ApplicationController
   end
 
   def create
-    @post_image = PostImage.new(post_image_prame)
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
 
   def index
@@ -21,6 +21,6 @@ class PostImagesController < ApplicationController
   private
 
   def post_image_params
-    parame.require(:post_image).permit(:shop.name, :image, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
